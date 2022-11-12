2cb885e8ffe7024188151dd64d739323e433ef04
Aleksandr Matsko <a.matsko@g.nsu.ru>
2022-11-12 14:10:12 +0700

    added lab 14, works for many threads, corrected lab 11

--- 11_sync_output/main.c
+++ 11_sync_output/main.c
@@ -73,7 +73,7 @@ int main(int argc, char *argv[]) {
     if (argc == 2) {
         MUTEX_NUM = atoi(argv[1]) + 1;
         if (MUTEX_NUM <= 1) {
-            MUTEX_NUM = 2;
+            MUTEX_NUM = 3;
         }
     }
     mutex = (pthread_mutex_t *) calloc(MUTEX_NUM, sizeof(pthread_mutex_t));
